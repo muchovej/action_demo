30353be30cb95d2890533120811d836209b39470
muchovej <stephen.muchovej@gmail.com>
2025-02-04 16:28:55 -0800

    -- add new test

--- tests/test_add.py
+++ tests/test_add.py
@@ -3,7 +3,7 @@ import pytest
 
 @pytest.mark.parametrize(
     "input1, input2, expected_value",
-    [(3, 2, 5), (-3, -2, -5), (-3, 2, -1), (4,5,9)],
+    [(3, 2, 5), (-3, -2, -5), (-3, 2, -1), (4,5,9), (5,10,15)],
 )
 def test_add(input1, input2, expected_value):
     assert add(input1, input2) == expected_value
